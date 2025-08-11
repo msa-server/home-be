9932a098380d936dc24f963b55c791e17e063685
BieNew22 <newbie11004@gmail.com>
2025-08-11 20:46:58 +0900

    init project

---
+++ src/test/java/com/web/bienew/BienewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.web.bienew;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BienewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
